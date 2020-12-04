253a750180712e1e5dc232b18fefa859d483c590
Konstantin Goncharov <kostjag15@gmail.com>
2020-12-04 19:37:18 +0300

    Added Appbar. Sort and Search moved to appbar.

--- app/src/main/java/com/hfad/notes/MainActivity.kt
+++ app/src/main/java/com/hfad/notes/MainActivity.kt
@@ -4,17 +4,55 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SearchView
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    private var fragment: Fragment? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar as Toolbar)
         view_pager.adapter = PagerAdapter(supportFragmentManager)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_notes, menu)
+        val searchItem = menu?.findItem(R.id.app_bar_search)
+        val searchView = searchItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.app_bar_sort) {
+            fragment = supportFragmentManager.findFragmentById(R.id.view_pager)
+            if (fragment is NotesFragment) (fragment as NotesFragment).sortNotes()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        fragment = supportFragmentManager.findFragmentById(R.id.view_pager)
+        if (fragment is NotesFragment && query != null)
+                (fragment as NotesFragment).searchNotes(query)
+        return false
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        if (newText.isNullOrEmpty()) {
+            fragment = supportFragmentManager.findFragmentById(R.id.view_pager)
+            if (fragment is NotesFragment) (fragment as NotesFragment).clearSearch()
+        }
+        return false
+    }
+
 
     private inner class PagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
         override fun getCount() = 1
--- app/src/main/java/com/hfad/notes/NotesFragment.kt
+++ app/src/main/java/com/hfad/notes/NotesFragment.kt
@@ -1,12 +1,10 @@
 package com.hfad.notes
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.*
-import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.core.view.forEach
 import androidx.core.view.get
@@ -15,9 +13,6 @@ import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_notes.*
 import kotlinx.android.synthetic.main.fragment_notes.view.*
 import kotlinx.android.synthetic.main.note_element.view.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 // TODO: Корзина заметок
 
@@ -25,8 +20,8 @@ class NotesFragment : Fragment() {
 
     private lateinit var helper: DatabaseHelper
     private lateinit var snackbar: Snackbar
+    private lateinit var notesAdapter: RecyclerNoteAdapter
     private val deletedNotes = mutableListOf<Note>()
-    private var savedSearchText: String? = null
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -37,7 +32,7 @@ class NotesFragment : Fragment() {
         helper = DatabaseHelper(notesRecycler.context)
         val standardAdapter = RecyclerNoteAdapter(helper.getNoteList()) // Настройка адаптера для RecyclerView
         notesRecycler.adapter = standardAdapter
-        val notesAdapter = notesRecycler.adapter as RecyclerNoteAdapter
+        notesAdapter = notesRecycler.adapter as RecyclerNoteAdapter
 
         val listener = object : RecyclerNoteAdapter.Listener {
             override fun onClick(position: Int) {
@@ -61,16 +56,12 @@ class NotesFragment : Fragment() {
                     }
                     override fun onLongClick(position: Int) {}
                 }
-                search_field.isEnabled = false
-                clear_search_btn.isEnabled = false
-                sort_btn.isEnabled = false
             }
         }
 
         notesAdapter.listener = listener
         val layoutManager = LinearLayoutManager(activity)
         notesRecycler.layoutManager = layoutManager
-
 
 
         val delOptionsListener = View.OnClickListener { btn -> // Поведение кнопок удаления
@@ -86,12 +77,8 @@ class NotesFragment : Fragment() {
                 }
                 notesAdapter.notifyDataSetChanged() // Обновление RecyclerVIew
 
-                clear_search_btn.isClickable = false
-                search_btn.isClickable = false
                 val handler = Handler(Looper.getMainLooper())
                 handler.postDelayed({
-                    search_btn.isClickable = true
-                    clear_search_btn.isClickable = true
                     deletedNotes.forEach {
                         helper.delNote(it.id)
                     }
@@ -118,77 +105,52 @@ class NotesFragment : Fragment() {
             delete_options.visibility = View.INVISIBLE
             add_note.visibility = View.VISIBLE
             standardAdapter.listener = listener
-            search_field.isEnabled = true
-            clear_search_btn.isEnabled = true
-            sort_btn.isEnabled = true
         }
         layout.delete_btn?.setOnClickListener(delOptionsListener)
         layout.back_btn?.setOnClickListener(delOptionsListener)
-
-
-
-        val otherButtonsListener = View.OnClickListener { v -> // Назначение действий для кнопок добавления и поиска
-            when (v?.id) {
-                R.id.add_note -> {
-                    val intent = Intent(activity, EditNoteActivity :: class.java) // Обработка нажатия кнопки добавления
-                    activity?.startActivity(intent)
-                }
-                R.id.search_btn -> {
-                    notesAdapter.notesParam = helper.getNoteList(search_field.text.toString().trim()) // Создание и присваивание адаптера с найденными заметками
-                    notesAdapter.notifyDataSetChanged()
-                    if (notesAdapter.notesParam.isEmpty()) no_notes_title.visibility = View.VISIBLE
-                }
-                R.id.sort_btn -> {
-                    val dialog = SortDialog()
-                    val ft = fragmentManager?.beginTransaction()
-                    if (ft != null) {
-                        dialog.show(ft, "dialog")
-                        dialog.dialogListener = object : SortDialog.DialogListener {
-                            override fun changeSort(type: SortDialog.SortType?) {
-                                when (type) {
-                                    SortDialog.SortType.AlphabetAscend ->
-                                        notesAdapter.notesParam.sortBy { it.title }
-                                    SortDialog.SortType.AlphabetDescend ->
-                                        notesAdapter.notesParam.sortByDescending { it.title }
-                                    SortDialog.SortType.DateAscend ->
-                                        notesAdapter.notesParam.sortBy { it.lastEditDate }
-                                    SortDialog.SortType.DateDescend ->
-                                        notesAdapter.notesParam.sortByDescending { it.lastEditDate }
-                                }
-                                notesAdapter.notifyDataSetChanged()
-                            }
-                        }
-                    }
-                }
-                R.id.clear_search_btn -> {
-                    notesAdapter.notesParam = helper.getNoteList()
-                    notesAdapter.notifyDataSetChanged()
-                    if (notesAdapter.notesParam.isNotEmpty())
-                        no_notes_title.visibility = View.INVISIBLE
-                    else no_notes_title.visibility = View.VISIBLE
-                    search_field.text.clear()
-                }
-            }
+        layout.add_note.setOnClickListener {
+            val intent = Intent(activity, EditNoteActivity :: class.java) // Обработка нажатия кнопки добавления
+            activity?.startActivity(intent)
         }
-
-
-
-        notesRecycler.setOnTouchListener { v, event -> // При касании снять фокус с search_field и спрятать клавиатуру
-            if (event.action == MotionEvent.ACTION_UP) {
-                layout.search_field.clearFocus()
-                (activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
-                        .hideSoftInputFromWindow(layout.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
-                v.performClick()
-            }
-            false
-        }
-        layout.add_note.setOnClickListener(otherButtonsListener)
-        layout.search_btn.setOnClickListener(otherButtonsListener)
-        layout.clear_search_btn.setOnClickListener(otherButtonsListener)
-        layout.sort_btn.setOnClickListener(otherButtonsListener)
         return layout
     }
 
+
+    fun searchNotes(text: String) {
+        notesAdapter.notesParam = helper.getNoteList(text)
+        notesAdapter.notifyDataSetChanged()
+        if (notesAdapter.notesParam.isEmpty()) no_notes_title.visibility = View.VISIBLE
+    }
+
+    fun clearSearch() {
+        notesAdapter.notesParam = helper.getNoteList()
+        notesAdapter.notifyDataSetChanged()
+        if (notesAdapter.notesParam.isNotEmpty()) no_notes_title.visibility = View.INVISIBLE
+        else no_notes_title.visibility = View.VISIBLE
+    }
+
+    fun sortNotes() {
+        val dialog = SortDialog()
+        val ft = fragmentManager?.beginTransaction()
+        if (ft != null) {
+            dialog.show(ft, "dialog")
+            dialog.dialogListener = object : SortDialog.DialogListener {
+                override fun changeSort(type: SortDialog.SortType?) {
+                    when (type) {
+                        SortDialog.SortType.AlphabetAscend ->
+                            notesAdapter.notesParam.sortBy { it.title }
+                        SortDialog.SortType.AlphabetDescend ->
+                            notesAdapter.notesParam.sortByDescending { it.title }
+                        SortDialog.SortType.DateAscend ->
+                            notesAdapter.notesParam.sortBy { it.lastEditDate }
+                        SortDialog.SortType.DateDescend ->
+                            notesAdapter.notesParam.sortByDescending { it.lastEditDate }
+                    }
+                    notesAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+    }
 
     override fun onStart() {
         super.onStart()
@@ -199,16 +161,9 @@ class NotesFragment : Fragment() {
             deletedNotes.clear()
             snackbar.dismiss()
         }
-        val notesAdapter = view?.recycler_notes?.adapter as RecyclerNoteAdapter
         notesAdapter.syncWithDatabase(recycler_notes.context) // Обновление RecyclerView
         if (notesAdapter.notesParam.isEmpty())
             no_notes_title.visibility = View.VISIBLE
         else no_notes_title.visibility = View.INVISIBLE
-    }
-
-
-    override fun onStop() {
-        super.onStop()
-        if (search_field.text.isNotBlank()) savedSearchText = search_field.text.toString()
     }
 }
